edff2bc90538c6e35d42892a35ac17524f9352e2
Laurens-Hertzer <laurens.hertzer@stud.kbw.ch>
2026-07-19 18:17:05 +0200

    feat: particle/stars background in hero canvas with light/dark mode

--- script.js
+++ script.js
@@ -1,140 +1,299 @@
 const button = document.getElementById('theme-toggle');
-
 button.addEventListener('click', () => {
-    document.body.classList.toggle('light-mode'); // Anschalten des light-class bei Body
+    document.body.classList.toggle('light-mode');
 });
 
-/* ---- particles.js config ---- */
-
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 70,
-      "density": {
-        "enable": true,
-        "value_area": 900
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      /*"stroke": {
-        "width": 0,
-        "color": "#000000"
-      },*/
-      /*"polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },*/
-    /*"opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }*/
-    },
-    "size": {
-      "value": 2.5,
-      "random": true,
-      /*"anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }*/
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 140,
-      "color": "#ffffff",
-      "opacity": 0.15,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 0.7,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      /*"bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }*/
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": false,
-        /*"mode": "push"*/
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 170,
-        "line_linked": {
-          "opacity": 0.35
-        }
-      }
-      /*"bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }*/
-    }
-  },
-  "retina_detect": true
-});
-
-
-/* ---- stats.js config ---- 
-
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
+/*
+Dieser Teil des Codes (bis zum nächsten Kommentar) ist Code von Ben Scott aus
+diesem Repo https://github.com/bscottnz/portfolio-site. Der Code ist zuständig
+für den Sterne/Partikel Hintergrund.
 */
+
+const canvasDots = function () {
+    const canvas = document.querySelector('canvas'),
+        ctx = canvas.getContext('2d'),
+        colorDot = [
+            'rgb(81, 162, 233)',
+            'rgb(81, 162, 233)',
+            'rgb(81, 162, 233)',
+            'rgb(81, 162, 233)',
+            'rgb(255, 77, 90)',
+        ], // 80% blue, 20% pink
+        color = 'rgb(81, 162, 233)';
+
+    canvas.width = document.body.scrollWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.display = 'block';
+    ctx.lineWidth = 0.3;
+    ctx.strokeStyle = color;
+
+    let mousePosition = {
+        x: (30 * canvas.width) / 100,
+        y: (30 * canvas.height) / 100,
+    };
+
+    const windowSize = window.innerWidth;
+    let dots;
+
+    if (windowSize > 1600) {
+        dots = { nb: 600, distance: 70, d_radius: 300, array: [] };
+    } else if (windowSize > 1300) {
+        dots = { nb: 575, distance: 60, d_radius: 280, array: [] };
+    } else if (windowSize > 1100) {
+        dots = { nb: 500, distance: 55, d_radius: 250, array: [] };
+    } else if (windowSize > 800) {
+        dots = { nb: 300, distance: 0, d_radius: 0, array: [] };
+    } else if (windowSize > 600) {
+        dots = { nb: 200, distance: 0, d_radius: 0, array: [] };
+    } else {
+        dots = { nb: 100, distance: 0, d_radius: 0, array: [] };
+    }
+
+    function Dot() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.vx = -0.5 + Math.random();
+        this.vy = -0.5 + Math.random();
+        this.radius = Math.random() * 1.5;
+        this.colour = colorDot[Math.floor(Math.random() * colorDot.length)];
+    }
+
+    Dot.prototype = {
+        create: function () {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            const dotDistance =
+                ((this.x - mousePosition.x) ** 2 + (this.y - mousePosition.y) ** 2) ** 0.5;
+            const distanceRatio = dotDistance / (windowSize / 1.7);
+            ctx.fillStyle = this.colour.slice(0, -1) + `,${1 - distanceRatio})`;
+            ctx.fill();
+        },
+
+        animate: function () {
+            for (let i = 1; i < dots.nb; i++) {
+                const dot = dots.array[i];
+                if (dot.y < 0 || dot.y > canvas.height) {
+                    dot.vy = -dot.vy;
+                } else if (dot.x < 0 || dot.x > canvas.width) {
+                    dot.vx = -dot.vx;
+                }
+                dot.x += dot.vx;
+                dot.y += dot.vy;
+            }
+        },
+
+        line: function () {
+            for (let i = 0; i < dots.nb; i++) {
+                for (let j = 0; j < dots.nb; j++) {
+                    const i_dot = dots.array[i];
+                    const j_dot = dots.array[j];
+                    if (
+                        i_dot.x - j_dot.x < dots.distance &&
+                        i_dot.y - j_dot.y < dots.distance &&
+                        i_dot.x - j_dot.x > -dots.distance &&
+                        i_dot.y - j_dot.y > -dots.distance
+                    ) {
+                        if (
+                            i_dot.x - mousePosition.x < dots.d_radius &&
+                            i_dot.y - mousePosition.y < dots.d_radius &&
+                            i_dot.x - mousePosition.x > -dots.d_radius &&
+                            i_dot.y - mousePosition.y > -dots.d_radius
+                        ) {
+                            ctx.beginPath();
+                            ctx.moveTo(i_dot.x, i_dot.y);
+                            ctx.lineTo(j_dot.x, j_dot.y);
+                            const dotDistance =
+                                ((i_dot.x - mousePosition.x) ** 2 + (i_dot.y - mousePosition.y) ** 2) ** 0.5;
+                            let distanceRatio = dotDistance / dots.d_radius;
+                            distanceRatio -= 0.3;
+                            if (distanceRatio < 0) distanceRatio = 0;
+                            ctx.strokeStyle = `rgb(81, 162, 233, ${1 - distanceRatio})`;
+                            ctx.stroke();
+                            ctx.closePath();
+                        }
+                    }
+                }
+            }
+        },
+    };
+
+    function createDots() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < dots.nb; i++) {
+            dots.array.push(new Dot());
+            var dot = dots.array[i];
+            dot.create();
+        }
+        dots.array[0].radius = 1.5;
+        dots.array[0].colour = '#51a2e9';
+        dot.line();
+        dot.animate();
+    }
+
+    window.onmousemove = function (parameter) {
+        // clientX/Y statt pageX/Y, damit Scroll die Position nicht verschiebt
+        mousePosition.x = parameter.clientX;
+        mousePosition.y = parameter.clientY;
+        try {
+            dots.array[0].x = parameter.clientX;
+            dots.array[0].y = parameter.clientY;
+        } catch {}
+    };
+
+    mousePosition.x = window.innerWidth / 2;
+    mousePosition.y = window.innerHeight / 2;
+
+    const draw = setInterval(createDots, 1000 / 30);
+
+    window.onresize = function () {
+        clearInterval(draw);
+        canvasDots();
+    };
+};
+
+/* Ende des kopierten Code von Ben Scott */
+
+/*
+Dieser Teil des Codes (bis zum nächsten Kommentar) ist Code von Ben Scott aus
+diesem Repo https://github.com/bscottnz/portfolio-site. Der Code ist zuständig
+für den Sterne/Partikel Hintergrund.
+*/
+
+const canvasDotsBg = function () {
+    const canvas = document.querySelector('.canvas-2'),
+        ctx = canvas.getContext('2d'),
+        colorDot = [
+            'rgb(81, 162, 233)',
+            'rgb(81, 162, 233)',
+            'rgb(81, 162, 233)',
+            'rgb(255, 77, 90)',
+        ], // 75% blue, 25% pink
+        color = 'rgb(81, 162, 233)';
+
+    canvas.width = document.body.scrollWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.display = 'block';
+    ctx.lineWidth = 0.3;
+    ctx.strokeStyle = color;
+
+    let mousePosition = {
+        x: (30 * canvas.width) / 100,
+        y: (30 * canvas.height) / 100,
+    };
+
+    const windowSize = window.innerWidth;
+    let dots;
+
+    if (windowSize > 1600) {
+        dots = { nb: 100, distance: 0, d_radius: 0, array: [] };
+    } else if (windowSize > 1300) {
+        dots = { nb: 75, distance: 0, d_radius: 0, array: [] };
+    } else if (windowSize > 1100) {
+        dots = { nb: 50, distance: 0, d_radius: 0, array: [] };
+    } else {
+        dots = { nb: 1, distance: 0, d_radius: 0, array: [] };
+        ctx.globalAlpha = 0;
+    }
+
+    function Dot() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.vx = -0.5 + Math.random();
+        this.vy = -0.5 + Math.random();
+        this.radius = Math.random() * 1.5;
+        this.colour = colorDot[Math.floor(Math.random() * colorDot.length)];
+    }
+
+    Dot.prototype = {
+        create: function () {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            const top = (window.pageYOffset || document.scrollTop) - (document.clientTop || 0);
+            const dotDistance =
+                ((this.x - mousePosition.x) ** 2 + (this.y - mousePosition.y + top) ** 2) ** 0.5;
+            const distanceRatio = dotDistance / (windowSize / 2);
+            ctx.fillStyle = this.colour.slice(0, -1) + `,${1 - distanceRatio})`;
+            ctx.fill();
+        },
+
+        animate: function () {
+            for (let i = 1; i < dots.nb; i++) {
+                const dot = dots.array[i];
+                if (dot.y < 0 || dot.y > canvas.height) {
+                    dot.vy = -dot.vy;
+                } else if (dot.x < 0 || dot.x > canvas.width) {
+                    dot.vx = -dot.vx;
+                }
+                dot.x += dot.vx;
+                dot.y += dot.vy;
+            }
+        },
+
+        line: function () {
+            for (let i = 0; i < dots.nb; i++) {
+                for (let j = 0; j < dots.nb; j++) {
+                    const i_dot = dots.array[i];
+                    const j_dot = dots.array[j];
+                    if (
+                        i_dot.x - j_dot.x < dots.distance &&
+                        i_dot.y - j_dot.y < dots.distance &&
+                        i_dot.x - j_dot.x > -dots.distance &&
+                        i_dot.y - j_dot.y > -dots.distance
+                    ) {
+                        if (
+                            i_dot.x - mousePosition.x < dots.d_radius &&
+                            i_dot.y - mousePosition.y < dots.d_radius &&
+                            i_dot.x - mousePosition.x > -dots.d_radius &&
+                            i_dot.y - mousePosition.y > -dots.d_radius
+                        ) {
+                            ctx.beginPath();
+                            ctx.moveTo(i_dot.x, i_dot.y);
+                            ctx.lineTo(j_dot.x, j_dot.y);
+                            const dotDistance =
+                                ((i_dot.x - mousePosition.x) ** 2 + (i_dot.y - mousePosition.y) ** 2) ** 0.5;
+                            let distanceRatio = dotDistance / dots.d_radius;
+                            distanceRatio -= 0.3;
+                            if (distanceRatio < 0) distanceRatio = 0;
+                            ctx.strokeStyle = `rgb(81, 162, 233, ${1 - distanceRatio})`;
+                            ctx.stroke();
+                            ctx.closePath();
+                        }
+                    }
+                }
+            }
+        },
+    };
+
+    function createDots() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < dots.nb; i++) {
+            dots.array.push(new Dot());
+            var dot = dots.array[i];
+            dot.create();
+        }
+        dots.array[0].radius = 1.5;
+        dots.array[0].colour = '#51a2e9';
+        dot.animate();
+    }
+
+    window.onscroll = function () {
+        mousePosition.x = window.innerWidth / 2;
+        mousePosition.y = window.innerHeight / 2;
+        const top = (window.pageYOffset || document.scrollTop) - (document.clientTop || 0);
+        mousePosition.y += top;
+    };
+
+    const draw = setInterval(createDots, 1000 / 30);
+
+    window.onresize = function () {
+        clearInterval(draw);
+        canvasDotsBg();
+    };
+};
+
+/* Ende des kopierten Code von Ben Scott */
+
+window.onload = function () {
+    canvasDotsBg();
+    canvasDots();
+};
